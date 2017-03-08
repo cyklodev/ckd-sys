bcd81203d629e370e57c189d368545acd590d576
Cyklodev <zephilou@cyklodev.com>
2017-03-08 21:15:09 +0100

    Update include.sh

--- include.sh
+++ include.sh
@@ -5,10 +5,27 @@ then
 	echo "Include functions and alias in .bashrc"
 	cp -v alias.inc ~/
 	cp -v functions.inc ~/
-	echo ". ~/alias.inc" >> ~/.bashrc
-	echo ". ~/functions.inc" >> ~/.bashrc
-	echo " Reload .bashrc ..."
-	. ~/.bashrc
+	
+	RC=`cat ~/.bashrc | grep alias.inc | wc -l` 
+	if [[ $RC -ne 0 ]]
+	then
+		echo 'alias.inc already defined in .bashrc';	
+	else
+		echo 'alias.inc added in .bashrc ';	
+		echo "source ~/.alias.inc" >> ~/.bashrc
+	fi
+	
+	RC=`cat ~/.bashrc | grep functions.inc | wc -l `
+        if [[ $RC -ne 0 ]]
+        then
+                echo 'functions.inc already defined in .bashrc';
+        else
+                echo 'functions.inc added in .bashrc ';
+		echo "source ~/functions.inc" >> ~/.bashrc
+        fi
+
+	echo " Reload .bashrc by sourcing it : "
+	echo "source ~/.bashrc"
 else 
-	echo " .profile not writable :["
+	echo " .bashrc not writable :["
 fi
