d56411f0010750e01fdd64b8c2f8302b9908eed8
Cyklodev <zephilou@cyklodev.com>
2017-03-13 20:10:37 +0100

    Add ssh password less and clean file name

--- bashrc-include.sh
+++ bashrc-include.sh
@@ -3,8 +3,8 @@
 if [[ -w ~/.bashrc ]]
 then 
 	echo "Include functions and alias in .bashrc"
-	cp -v alias.inc ~/
-	cp -v functions.inc ~/
+	cp -v alias ~/.alias.inc
+	cp -v functions ~/.functions.inc
 	
 	RC=`cat ~/.bashrc | grep alias.inc | wc -l` 
 	if [[ $RC -ne 0 ]]
@@ -12,7 +12,7 @@ then
 		echo 'alias.inc already defined in .bashrc';	
 	else
 		echo 'alias.inc added in .bashrc ';	
-		echo "source ~/alias.inc" >> ~/.bashrc
+		echo "source ~/.alias.inc" >> ~/.bashrc
 	fi
 	
 	RC=`cat ~/.bashrc | grep functions.inc | wc -l `
@@ -21,7 +21,7 @@ then
                 echo 'functions.inc already defined in .bashrc';
         else
                 echo 'functions.inc added in .bashrc ';
-		echo "source ~/functions.inc" >> ~/.bashrc
+		echo "source ~/.functions.inc" >> ~/.bashrc
         fi
 
 	echo " Reload .bashrc by sourcing it : "
